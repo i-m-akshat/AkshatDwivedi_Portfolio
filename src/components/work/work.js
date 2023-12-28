e71ff074daeae902640678e48a28c43f1c339aec
AxA <akshatdwivedi59941@gmail.com>
2023-12-29 02:18:18 +0530

    Update work.js

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -9,21 +9,21 @@ export default function Work(){
             title:"Online Library Management",
             img:"Assets/OnlineLibraryManagement.png",
             address:"https://github.com/Akshat4756/OnlineLibraryManagement",
-            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            description:"This is a web application which i have created using Asp.net , Ado.net and Sql server. This application was developed with the purpose of providing an allrounder application to help a particular library to manage all their activities in a completely new way."
         },
         {
             id:2,
             title:"Ayodhya Darshnam",
             img:"Assets/AyodhyaDarshnamFullpage.jpeg",
             address:"http://ayodhyadarshnam.in/",
-            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            description:"It is a web application which i have develeoped using asp.net , Sql server, C#, ado.net and bootstrap5. This application was developed with the intent of promoting tourism to my own city Ayodhya. I am currently working in order to make it more better and optimize. It mainly have seperate logins of user and Admins and a seperate dashboard for admin to add/update the new spots, images or any kind of events"
         },
         {
             id:3,
             title:"Project MCQ",
             img:"Assets/ProjectMCQFullPage.jpeg",
             address:"https://github.com/Akshat4756/Project_MCQ",
-            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+            description:"This web application is built using Asp.net, Ado.net and uses Sql server as database. I created this project in the process of learning asp.net"
         },
         // {
         //     id:4,

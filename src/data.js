993e05277c31ddd8d74598a7043dbab544375923
Akshat <akshatdwivedi59941@gmail.com>
2023-12-29 03:10:59 +0530

    CommitFixes

--- src/data.js
+++ src/data.js
@@ -1,111 +1,120 @@
 export const featurePortfolio =[
     {
         id:1,
-        title:"Social Media App",
-        img:"Assets/undraw_progressive_app_m9ms.png"
+        title:"Ayodhya Darshnam",
+        img:"Assets/AyodhyaDarshnamFullpage.jpeg"
     },
     {
         id:2,
-        title:"Dummy 2",
-        img:"Assets/undraw_progressive_app_m9ms.png"
+        title:"Online Food Application",
+        img:"Assets/FullScreenShot.jpeg"
     },
     {
         id:3,
-        title:"Dummy 3",
-        img:"Assets/undraw_progressive_app_m9ms.png"
+        title:"Portfolio",
+        img:"Assets/PortfolioFullSS.jpeg"
     },
     {
         id:4,
-        title:"Dummy 4",
-        img:"Assets/undraw_progressive_app_m9ms.png"
+        title:"Project MCQ",
+        img:"Assets/ProjectMCQFullPage.jpeg"
     },
     {
         id:5,
-        title:"Dummy 5",
-        img:"Assets/undraw_progressive_app_m9ms.png"
+        title:"Online Library Management",
+        img:"Assets/OnlineLibraryManagement.png"
     }
     
 ];
 export const Design=[
     {
         id:1,
-        title:"Social Media App",
-        img:"Assets/Niche service marketplace-pana.png"
+        title:"Ayodhya Darshnam",
+        img:"Assets/AyodhyaDarshnamFullpage.jpeg"
     },
     {
         id:2,
-        title:"Dummy 2",
-        img:"Assets/Niche service marketplace-pana.png"
+        title:"Online Food Application",
+        img:"Assets/FullScreenShot.jpeg"
     },
     {
         id:3,
-        title:"Dummy 3",
-        img:"Assets/Niche service marketplace-pana.png"
+        title:"Portfolio",
+        img:"Assets/PortfolioFullSS.jpeg"
     },
     {
         id:4,
-        title:"Dummy 4",
-        img:"Assets/Niche service marketplace-pana.png"
+        title:"Project MCQ",
+        img:"Assets/ProjectMCQFullPage.jpeg"
     },
     {
         id:5,
-        title:"Dummy 5",
-        img:"Assets/Niche service marketplace-pana.png"
+        title:"Online Library Management",
+        img:"Assets/OnlineLibraryManagement.png"
+    },{
+        id:6,
+        title:"Calculator",
+        img:"Assets/Calculator.png"
+    },
+    {
+        id:7,
+        title:"Image Slider Application",
+        img:"Assets/ImageSlider.png"
     }
-    
 ];
 export const MobileApp=[
     {
         id:1,
-        title:"Dummy1",
-        img:"Assets/Online wishes list-pana.png"
+        title:"Calculator",
+        img:"Assets/Calculator.png"
     },
     {
         id:2,
-        title:"Dummy 2",
-        img:"Assets/Online wishes list-pana.png"
-    },
-    {
-        id:3,
-        title:"Dummy 3",
-        img:"Assets/Online wishes list-pana.png"
-    },
-    {
-        id:4,
-        title:"Dummy 4",
-        img:"Assets/Online wishes list-pana.png"
-    },
-    {
-        id:5,
-        title:"Dummy 5",
-        img:"Assets/Online wishes list-pana.png"
+        title:"Image Slider Application",
+        img:"Assets/ImageSlider.png"
     }
+    // ,
+    // {
+    //     id:3,
+    //     title:"Dummy 3",
+    //     img:"Assets/Online wishes list-pana.png"
+    // },
+    // {
+    //     id:4,
+    //     title:"Dummy 4",
+    //     img:"Assets/Online wishes list-pana.png"
+    // },
+    // {
+    //     id:5,
+    //     title:"Dummy 5",
+    //     img:"Assets/Online wishes list-pana.png"
+    // }
 ];
 export const WebApplications=[
     {
         id:1,
-        title:"Dummy1",
-        img:"Assets/Ecommerce web page-pana.png"
+        title:"Ayodhya Darshnam",
+        img:"Assets/AyodhyaDarshnamFullpage.jpeg"
     },
     {
         id:2,
-        title:"Dummy 2",
-        img:"Assets/Ecommerce web page-pana.png"
+        title:"Online Food Application",
+        img:"Assets/FullScreenShot.jpeg"
     },
     {
         id:3,
-        title:"Dummy 3",
-        img:"Assets/Ecommerce web page-pana.png"
+        title:"Portfolio",
+        img:"Assets/PortfolioFullSS.jpeg"
     },
     {
         id:4,
-        title:"Dummy 4",
-        img:"Assets/Ecommerce web page-pana.png"
+        title:"Project MCQ",
+        img:"Assets/ProjectMCQFullPage.jpeg"
     },
     {
         id:5,
-        title:"Dummy 5",
-        img:"Assets/Ecommerce web page-pana.png"
+        title:"Online Library Management",
+        img:"Assets/OnlineLibraryManagement.png"
     }
 
 ];
